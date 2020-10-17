922bf204144b72cb7f74bed695c1f2feb3cebf53
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2020-10-17 14:54:48 -0300

    Updates

---
+++ precache-manifest.950cfe54e76243c1994c47562b826033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953acc6e528fa9ea9f757118e2ee98bc",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "f397d6b2aa8a448a43aa",
+    "url": "/Pomodoro-Clock/static/css/main.02b57bc0.chunk.css"
+  },
+  {
+    "revision": "5d4d700975a4d4155ec0",
+    "url": "/Pomodoro-Clock/static/js/2.4c7eeff7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Clock/static/js/2.4c7eeff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f397d6b2aa8a448a43aa",
+    "url": "/Pomodoro-Clock/static/js/main.531cf161.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  },
+  {
+    "revision": "14bf5171ddadcf146feea894b1dca00c",
+    "url": "/Pomodoro-Clock/static/media/pomodoro.img.14bf5171.png"
+  }
+]);
